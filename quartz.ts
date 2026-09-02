016009225694846f04ede1f953a2091e6c5e435f
greenjohn40 <green.john40@gmail.com>
2026-09-02 08:46:40 +0200

    Update quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,22 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+const customOrder = ["matematika", "fyzika", "hodnocení", "o-sekci"]
+
+ExternalPlugin.Explorer({
+  sortFn: (a, b) => {
+    const indexA = customOrder.indexOf(a.name)
+    const indexB = customOrder.indexOf(b.name)
+
+    if (indexA !== -1 && indexB !== -1) {
+      return indexA - indexB
+    }
+    if (indexA === -1 && indexB === -1) {
+      return a.displayName.localeCompare(b.displayName)
+    }
+    return indexA === -1 ? 1 : -1
+  },
+})
 
 const config = await loadQuartzConfig()
 export default config
